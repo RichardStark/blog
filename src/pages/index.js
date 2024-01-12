930b8bcfc463a2d2604ebab245f6d2a909751df5
RichardStark <yantaiv@gmail.com>
2024-01-12 14:00:03 +0800

    image plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return(
     <main>
       <Layout pageTitle='首页'>
         <p>这是一个测试页。</p>
+        <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/fh_video.jpg"
+        />
       </Layout>
     </main>
   )
