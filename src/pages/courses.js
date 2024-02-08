070b3d1aba45f9e710e632aae453622cbd9b513f
RichardStark <yantaiv@gmail.com>
2024-02-08 16:08:15 +0800

    fix svg

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -13,23 +13,26 @@ const CoursesPage = ({ data }) => {
             Since 2020, my primary focus has been on creating interactive
             self-paced online courses. Here's what I've produced so far.
           </p>
-          <div className="overflow-hidden">
-            <svg
-              preserveAspectRatio="none"
-              width="1440"
-              height="74"
-              viewBox="0 0 1440 74"
-              className="w-full fill-blue-300"
-            >
-              <path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path>
-            </svg>
-          </div>
           {data.allMdx.nodes.map((node) => (
-            <article key={node.id} className="-mb-1">
+            <article key={node.id}>
+              <div>
+                <svg
+                  viewBox="0 0 1433 176"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="none"
+                  className="w-full fill-blue-300"
+                >
+                  <path d="M638 40C394.315 26.0653 1.5 123.5 1.5 123.5V175.5H1433V51.5C1433 51.5 1177.5 136.5 1036.5 123.5C847.439 106.069 827.553 50.8392 638 40Z"></path>
+                </svg>
+              </div>
               <div className="bg-blue-300">
                 <div className="max-w-3xl pt-0 pb-8 relative mx-auto pl-8 pr-8">
-                <img alt="Cute cardboard box mascot" src="https://www.joshwcomeau.com/images/css-for-js-mascot-light.png?v=2"
-                  className="w-64 h-56 -mt-16 -mr-20 float-right"/>
+                  <img
+                    alt="Cute cardboard box mascot"
+                    src="https://www.joshwcomeau.com/images/css-for-js-mascot-light.png?v=2"
+                    className="w-64 h-56 -mt-28 -mr-20 float-right"
+                  />
                   <h2 className="text-3xl">
                     <Link to={`/blog/${node.frontmatter.slug}`}>
                       {node.frontmatter.title}
@@ -52,17 +55,15 @@ const CoursesPage = ({ data }) => {
                   </div>
                 </div>
               </div>
-              <div class="sc-f37ea003-14 drzVpJ">
+              <div>
                 <svg
-                  viewBox="0 0 1433 176"
+                  viewBox="0 0 1433 110"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                   preserveAspectRatio="none"
-                  class="sc-f37ea003-15 sc-f37ea003-17 iqcpxh fOXOfW"
                   className="w-full fill-blue-300"
                 >
                   <path d="M0 93V0H1433C1433 0 1220.5 103.5 1111 103.5C872.927 103.5 805.531 18.0095 567.5 22.5C344.214 26.7124 0 93 0 93Z"></path>
-                  <path d="M638 40C394.315 26.0653 1.5 123.5 1.5 123.5V175.5H1433V51.5C1433 51.5 1177.5 136.5 1036.5 123.5C847.439 106.069 827.553 50.8392 638 40Z"></path>
                 </svg>
               </div>
             </article>
